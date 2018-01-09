a444d48d360c87d3dfe1930f5bda2fae1367c136
jianchang69 <jianchang69@gmail.com>
2018-01-09 11:48:50 +0800

    3rd

--- jimChat.php
+++ jimChat.php
@@ -1,8 +1,10 @@
 <?php
+  echo "hello!"
   $json_str = file_get_contents('php://input'); //接收request之body
   $json_obj = json_decode($json_str); //轉json format
 
   $myfile = fopen("log.txt","w+") or die( "unable to open file"); //設一個log.txt用來印訊息
   fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前加入 \xEF\xBB\xBf 轉成utf8
   fclose($myfile);  
+  echo "hello2!"
 ?>
