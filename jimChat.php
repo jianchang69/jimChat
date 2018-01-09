c018d795312c18d1e3bdf7084752c3421ccfec0e
jianchang69 <jianchang69@gmail.com>
2018-01-09 11:46:30 +0800

    2nd

--- jimChat.php
+++ jimChat.php
@@ -4,6 +4,5 @@
 
   $myfile = fopen("log.txt","w+") or die( "unable to open file"); //設一個log.txt用來印訊息
   fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前加入 \xEF\xBB\xBf 轉成utf8
-  fclose($myfile);
-  
+  fclose($myfile);  
 ?>
